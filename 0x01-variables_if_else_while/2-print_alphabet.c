2c6350d5e48a6b5adbf1e19e29b9d2d9826e2565
samezs <99283383+samezs@users.noreply.github.com>
2022-03-11 23:32:51 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: print alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
