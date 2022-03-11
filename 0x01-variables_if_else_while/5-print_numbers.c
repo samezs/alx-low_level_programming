cae883f062123cd01cb032f2523e3797e42b9825
samezs <99283383+samezs@users.noreply.github.com>
2022-03-11 23:34:32 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry block
+ * Description: print single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
